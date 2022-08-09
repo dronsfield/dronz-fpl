5cef08b0fcefbbe345f063147569f1a565d5b3eb
Josh Dronsfield <josh.dronsfield@gmail.com>
2022-08-09 11:26:02 +0100

    debug event status error

--- app/util/betterFetch.ts
+++ app/util/betterFetch.ts
@@ -51,25 +51,34 @@ async function betterFetch<T>(
   url: string,
   options?: { contentType?: ContentType; [key: string]: any }
 ): Promise<T> {
-  if (!options) options = {}
-  let { contentType, ...otherOptions } = options
-  if (!contentType) contentType = "json"
-  const contentTypeHeader = getContentTypeHeader(contentType)
-  const headers = { ...options.headers }
-  if (contentTypeHeader) headers["Content-Type"] = contentTypeHeader
-  const fetchOptions = { ...otherOptions, headers }
-  const resp = await fetch(url, fetchOptions)
-  const data = await getResponseData(resp, contentType)
-  if (!resp.ok) {
-    const error = new BetterFetchError({
-      url,
-      method: options.method || "GET",
-      status: resp.status,
-      data
-    })
-    throw error
+  let resp: Response | null = null
+  try {
+
+    if (!options) options = {}
+    let { contentType, ...otherOptions } = options
+    if (!contentType) contentType = "json"
+    const contentTypeHeader = getContentTypeHeader(contentType)
+    const headers = { ...options.headers }
+    if (contentTypeHeader) headers["Content-Type"] = contentTypeHeader
+    const fetchOptions = { ...otherOptions, headers }
+    const resp = await fetch(url, fetchOptions)
+    const data = await getResponseData(resp, contentType)
+    if (!resp.ok) {
+      const error = new BetterFetchError({
+        url,
+        method: options.method || "GET",
+        status: resp.status,
+        data
+      })
+      throw error
+    }
+    return data
+  } catch (err: any) {
+    if (err && resp) {
+      console.log("resp body:", String((resp as any).body))
+    }
+    throw err
   }
-  return data
 }
 
 export default betterFetch
