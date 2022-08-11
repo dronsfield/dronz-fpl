0a47adee28ee7b0f8898cf95fc0c64ed40533b01
Josh Dronsfield <josh.dronsfield@gmail.com>
2022-08-11 23:38:33 +0100

    debugging css

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -69,10 +69,12 @@ const cellStyle = css<CellProps>`
 `;
 
 const Cell = styled.td<CellProps>`
+  content: "debugging, normal cell";
   ${cellStyle};
 `;
 
 const HeaderCell = styled.th<CellProps & { onClick?: any }>`
+  content: "debugging, header cell";
   font-weight: bold;
   font-size: 10px;
   text-transform: uppercase;
