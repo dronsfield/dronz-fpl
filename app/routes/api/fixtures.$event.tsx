3c85ef9cb50f5296676fcd93b144ce624800bdfd
Josh <josh.dronsfield@gmail.com>
2023-01-03 18:01:24 +0000

    add favicons

--- app/routes/api/fixtures.$event.tsx
+++ app/routes/api/fixtures.$event.tsx
@@ -1,16 +1,16 @@
-import {LoaderFunction} from "remix";
-import {EventStatusRT, FixtureRT} from "~/services/api/requests";
-import {runtypeFetch} from "~/util/runtypeFetch";
+import { LoaderFunction } from "remix";
+import { EventStatusRT, FixtureRT } from "~/services/api/requests";
+import { runtypeFetch } from "~/util/runtypeFetch";
 import appConfig from "~/appConfig";
-import {Array} from "runtypes";
-import {randomKey} from "~/util/randomKey";
+import { Array } from "runtypes";
+import { randomKey } from "~/util/randomKey";
 
-export const loader: LoaderFunction = async ({params}) => {
+export const loader: LoaderFunction = async ({ params }) => {
   const url = `${appConfig.BASE_URL}/fixtures/?event=${params.event}`;
   const rt = Array(FixtureRT);
-  const fixtures = await runtypeFetch(rt, url)
-  fixtures.forEach(x => {
-    x.team_h_score = Math.floor(Math.random()*100)
-  })
-  return fixtures
-}
+  const fixtures = await runtypeFetch(rt, url);
+  // fixtures.forEach(x => {
+  //   x.team_h_score = Math.floor(Math.random()*100)
+  // })
+  return fixtures;
+};
